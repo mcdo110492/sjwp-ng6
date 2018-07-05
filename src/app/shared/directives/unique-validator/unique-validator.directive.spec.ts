85fae5ed511542f0130bdaed29146bbf6f817ff8
fuentebellamcdonald@gmail.com <fuentebellamcdonald@gmail.com>
2018-07-05 17:06:11 +0800

    Remove All the spec files

--- src/app/shared/directives/unique-validator/unique-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UniqueValidatorDirective } from './unique-validator.directive';
-
-describe('UniqueValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new UniqueValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
